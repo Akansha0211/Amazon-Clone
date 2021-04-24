51d1576d7b214216e9b1053907fcc54fe67437f2
Akansha0211 <akansha.saxena0211@gmail.com>
2021-04-24 12:33:50 +0530

    Firebase config done

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA6AbQtp590DesVXsW0N2XkuRmGOsVPcEk",
+    authDomain: "challenge-9a753.firebaseapp.com",
+    projectId: "challenge-9a753",
+    storageBucket: "challenge-9a753.appspot.com",
+    messagingSenderId: "842984261664",
+    appId: "1:842984261664:web:6b24d6e94ab30a5aee1445",
+    measurementId: "G-S4TMZC1X1V"
+  };
+
+  
