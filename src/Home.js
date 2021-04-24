56af206d9e187db5057c69a7653ade032ec1a024
Akansha0211 <akansha.saxena0211@gmail.com>
2021-04-24 17:49:11 +0530

    Styling home component using Product component

---
+++ src/Home.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './Home.css';
+import Product from './Product';
+
+function Home() {
+    return (
+        <div className="home">
+            <div className="home__container">
+                <img className="home__image" src="https://images-eu.ssl-images-amazon.com/images/G/31/img21/Beauty/GW/April/2021-SSD-Herotator-1500x600._CB670253033_.jpg" alt="home-img"></img>
+
+                <div className="home__row">
+                    {/* product component */}
+                    <Product/>
+                    <Product/>
+                    <Product/>
+                    
+                    
+                </div>
+                <div className="home__row">
+                    {/* product component */}
+                    {/* product component */}
+                    {/* product component */}
+                    {/* product component */}
+                </div>
+                <div className="home__row" >
+                    {/* product component */}
+                    {/* product component */}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
