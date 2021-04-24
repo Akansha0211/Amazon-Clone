157758a362b608de6505dc7f76358afe9e255d77
Akansha0211 <akansha.saxena0211@gmail.com>
2021-04-25 02:01:25 +0530

    Setting up layout and styling for shopping basket and subtotal elelnet on checkout page on the right

--- src/Checkout.js
+++ src/Checkout.js
@@ -4,7 +4,23 @@ import  './Checkout.css'
 function Checkout() {
     return (
         <div className="checkout" >
-            <h1>Hello Checkout Page</h1>
+            <div className="checkout__left">
+                <img className="checkout__ad" src="https://images-eu.ssl-images-amazon.com/images/G/31/img17/Home/LA/Diwali2019/Rishab/Auto_Biss/BISS/1500x300_sanitizer.jpg" ></img>
+                <div>
+                    <h2 className="checkout_title">
+                        Your Shopping Basket
+                    </h2>
+
+                    {/* Basket Item */}
+                    {/* Basket Item */}
+                    {/* Basket Item */}
+
+                </div>
+            </div>
+            <div className="checkout__right" >
+                {/* subtotal component */}
+                <h2>The subtotal will go here</h2>
+            </div>
         </div>
     )
 }
