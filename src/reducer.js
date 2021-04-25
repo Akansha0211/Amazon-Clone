cf7d49cf4e7d523cbe65a45c2e55815c81583546
Akansha0211 <akansha.saxena0211@gmail.com>
2021-04-25 18:35:11 +0530

    FIx bug in removing element from the basket

--- src/reducer.js
+++ src/reducer.js
@@ -15,10 +15,16 @@ const reducer = (state, action)=>{
                 basket:[...state.basket, action.item]
             };
         case 'REMOVE_FROM_BASKET':
-            return{
-                  ...state,
-                  basket :state.basket.filter(item=>item.id !== action.id)
-
+            const index = state.basket.findIndex((basketItem)=>basketItem.id === action.id);
+            let newBasket = [...state.basket];
+            if(index >=0){
+                newBasket.splice(index,1);
+            }else{
+                console.warn(`Cant remove product ${action.id} as it is nnot in basket`)
+            }
+            return {
+                ...state,
+                basket : newBasket
             }
         default:
             return state;
