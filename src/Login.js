3c746557368bd9458b11aa1854ab1f69393ca214
Akansha0211 <akansha.saxena0211@gmail.com>
2021-04-25 19:29:29 +0530

    Add styling to the login page

--- src/Login.js
+++ src/Login.js
@@ -4,11 +4,28 @@ import './Login.css';
 
 function Login() {
     return (
-        <div className="Login" >
+        <div className="login" >
             <Link to="/">
                 <img className="login__logo" src="https://www.logo.wine/a/logo/Amazon_(company)/Amazon_(company)-Logo.wine.svg" />
             </Link>
-            
+            <div className="login__container" >
+                <h1>Sign in</h1>
+                <form>
+                    <h5>E-mail</h5>
+                    <input type="text"/>
+
+                    <h5>Password</h5>
+                    <input type="password"/>
+                    <button className="login__signInButton" >Sign In</button>
+                </form>
+                <p>
+                    By signing-in you agree to Amazon's
+                    Conditions of Use & Sale.Please see our Privacy Notice, our Cookies Notice
+                    and our Interest-Based Ads Notice.
+                </p>
+                <button className="login__registerButton" >Create your Amazon account</button>
+            </div>
+
         </div>
     )
 }
