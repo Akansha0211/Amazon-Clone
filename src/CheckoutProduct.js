939e4ef2f4d3f639a24120bbbd11be11deff635a
Akansha0211 <akansha.saxena0211@gmail.com>
2021-04-25 17:16:42 +0530

    Put items in the basket every time you click on Add to basket ,ie it maps every in the basket to display it on the checkout page

---
+++ src/CheckoutProduct.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import './CheckoutProduct.css';
+import StarIcon from '@material-ui/icons/Star';
+
+function CheckoutProduct({id,image,title,price,rating}) {
+    return (
+        <div className="checkoutProduct">
+            <img className="checkoutProduct__image" src={image}></img>
+            <div className="checkoutProduct__info" >
+                <p className="checkoutProduct__title" >{title}</p>
+                <p className="checkoutProduct__price" >
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="checkoutProduct__rating" >
+                    {Array(rating).fill().map((_,i)=>{
+                        <StarIcon/>
+                    })}
+                </div>
+                <button>Remove from Basket</button>
+            </div>
+        </div>
+    )
+}
+
+export default CheckoutProduct
